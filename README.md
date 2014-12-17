829d5e0c08b43258e3347401b8c7da7b4d877014
Chinmay Patwardhan <chinnychin19@gmail.com>
2014-12-17 17:45:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chat
+
+This is a *very* basic chat app made using Node.js and Socket.io. 
+
+Features:
+1. Nicknames
+2. Broadcast a message to all users
+3. Keeps track of how many users are connected
+4. That's it! I told you it was basic after all.
---
+++ app.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var numConnections = 0;
+
+io.on('connection', function(socket) {
+  numConnections += 1;
+  io.emit('numConnections', numConnections);
+
+    socket.broadcast.emit('chat message', {
+      name: "Chat Room",
+      msg: "Someone connected"
+    });
+
+  socket.on('chat message', function(obj) {
+    socket.broadcast.emit('chat message', obj);
+  });
+
+  socket.on('disconnect', function() {
+    numConnections -= 1
+    io.emit('numConnections', numConnections);
+
+    socket.broadcast.emit('chat message', {
+      name: "Chat Room",
+      msg: "Someone disconnected"
+    });
+  });
+
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
